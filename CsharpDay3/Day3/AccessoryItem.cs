78779c65175c1946e636d37da9e124cb77affa4a
unknown <pavithra@MUMBAI-AD.PLURALSIGHT.COM>
2024-07-23 23:44:30 +0530

    added

---
+++ CsharpDay3/Day3/AccessoryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDay3.Day3
+{
+    public class AccessoryItem : InventoryItem
+    {
+        public string Size { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ CsharpDay3/Day3/CageItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDay3.Day3
+{
+    public class CageItem: InventoryItem
+    {
+        public string Dimensions {  get; set; }
+        public string Material {  get; set; }
+    }
+}
