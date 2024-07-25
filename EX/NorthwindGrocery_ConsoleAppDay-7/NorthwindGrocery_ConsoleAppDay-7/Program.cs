181c02ba7a3d44cc5bad19f5875323ceab6a0b78
unknown <pavithra@MUMBAI-AD.PLURALSIGHT.COM>
2024-07-25 18:46:34 +0530

    add

---
+++ EX/NorthwindGrocery_ConsoleAppDay-7/NorthwindGrocery_ConsoleAppDay-7/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NorthwindGrocery_ConsoleAppDay_7;
+Console.WriteLine("Hello, World!");
+
+northwind nw = new northwind();
+bool flag = true;
+do
+{
+    Console.WriteLine("---------------------");
+    Console.WriteLine("Enter the option:");
+    Console.WriteLine("1.List all categories");
+    Console.WriteLine("2.List products in a specific category");
+    Console.WriteLine("3.List products in a price range");
+    Console.WriteLine("4.List all suppliers");
+    Console.WriteLine("5.List products from a specific category");
+    Console.WriteLine("6.Quit");
+    Console.WriteLine("---------------------");
+
+    int option = int.Parse(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            nw.getCategories();
+            Console.WriteLine("---------------------");
+            break;
+        case 2:
+            nw.displayCategories();
+            bool check = false;
+            do
+            {
+                Console.WriteLine("Select category to view its products");
+                string category = Console.ReadLine();
+                check = nw.CategoryExists(category);
+                Console.WriteLine(check);
+            } while (!check);
+            break;
+        case 6:
+            flag = false;
+            break;
+    }
+
+} while (flag);
+
+
---
+++ EX/NorthwindGrocery_ConsoleAppDay-7/NorthwindGrocery_ConsoleAppDay-7/northwind.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace NorthwindGrocery_ConsoleAppDay_7
+{
+    public class northwind
+    {   
+        public void getCategories()
+        {
+            SqlConnection con = null;
+            try
+            {
+                string connectionString = "Server=WSAMZN-FK6DHHFM;Database=demo;User Id=sa;Password=Password@123;";
+                // Creating Connection  
+                con = new SqlConnection(connectionString);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                string sqlQuery = "SELECT * FROM Categories";
+                SqlCommand command = new SqlCommand(sqlQuery, con);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["CategoryID"] + " " + reader["CategoryName"] + " " + reader["Description"]+" "+ reader["Picture"]);
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+        
+        public void displayCategories()
+        {
+            SqlConnection con = null;
+            try
+            {
+                string connectionString = "Server=WSAMZN-FK6DHHFM;Database=demo;User Id=sa;Password=Password@123;";
+                // Creating Connection  
+                con = new SqlConnection(connectionString);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                string sqlQuery = "SELECT CategoryName FROM Categories";
+                SqlCommand command = new SqlCommand(sqlQuery, con);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine( reader["CategoryName"]);
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public bool CategoryExists(string category)
+        {
+            SqlConnection con = null;
+            try
+            {
+                string connectionString = "Server=WSAMZN-FK6DHHFM;Database=demo;User Id=sa;Password=Password@123;";
+                // Creating Connection  
+                con = new SqlConnection(connectionString);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                string sqlQuery = "SELECT Count(*) as Count FROM Categories where CategoryName ="+ $"'{category}'";
+                Console.WriteLine(sqlQuery);
+                SqlCommand command = new SqlCommand(sqlQuery, con);
+                int rowCount = (int)command.ExecuteScalar();
+                if (rowCount > 0)
+                {
+                    return true;
+                }
+                    
+                else
+                {
+                    return false;
+                }
+               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+                return false;
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
